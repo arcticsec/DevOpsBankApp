ca7dec25c01477913fd66b2ee1fbe36eff74f9c0
arcticsec <mabj00@gmail.com>
2023-10-31 22:48:13 +0200

    V2.0

--- app.py
+++ app.py
@@ -1,21 +1,56 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = '12345678'  # Change this to a real secret key in production
 
-@app.route('/')
+# Hardcoded user credentials and their accounts
+users = {
+    'user1': {
+        'password': 'pass1',
+        'accounts': [
+            {'account_number': '00001', 'balance': 1000},
+            {'account_number': '00002', 'balance': 2500},
+        ]
+    },
+    'user2': {
+        'password': 'pass2',
+        'accounts': [
+            {'account_number': '00003', 'balance': 3000},
+        ]
+    }
+}
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    # If user is already logged in, redirect to the dashboard
+    if 'username' in session:
+        return redirect(url_for('dashboard'))
+    # Handle login
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = users.get(username)
+        if user and user['password'] == password:
+            session['username'] = username
+            return redirect(url_for('dashboard'))
+        else:
+            return 'Login Failed', 401
     return render_template('index.html')
 
-@app.route('/dashboard', methods=['POST'])
+@app.route('/dashboard')
 def dashboard():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    
-    # This is a basic check; in real applications, you'll need more secure ways to handle this
-    if username == "user" and password == "pass":
-        return render_template('dashboard.html', username=username)
-    else:
+    # Check if user is logged in
+    if 'username' not in session:
         return redirect(url_for('index'))
+    username = session['username']
+    user_accounts = users[username]['accounts']
+    return render_template('dashboard.html', username=username, accounts=user_accounts)
+
+@app.route('/logout')
+def logout():
+    # Remove user from session
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- test_app.py
+++ test_app.py
@@ -10,6 +10,26 @@ def app():
 def client(app):
     return app.test_client()
 
-def test_index_route(client):
-    response = client.get("/")
-    assert response.status_code == 200
+def test_login_success(client):
+    response = client.post("/", data={'username': 'user1', 'password': 'pass1'})
+    assert response.status_code == 302
+    assert '/dashboard' in response.headers['Location']
+
+def test_login_failure(client):
+    response = client.post("/", data={'username': 'user1', 'password': 'wrongpass'})
+    assert response.status_code == 401
+
+def test_dashboard_access(client):
+    with client:
+        client.post("/", data={'username': 'user1', 'password': 'pass1'})
+        response = client.get("/dashboard")
+        assert response.status_code == 200
+        assert b'00001' in response.data  # Check for account number
+
+def test_logout(client):
+    with client:
+        client.post("/", data={'username': 'user1', 'password': 'pass1'})
+        client.get('/logout')
+        response = client.get('/dashboard')
+        assert response.status_code == 302
+        assert '/' in response.headers['Location']
