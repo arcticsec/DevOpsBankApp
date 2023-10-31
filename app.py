bcce89e375458d26c0a0ea7c94499d21d30459d7
arcticsec <mabj00@gmail.com>
2023-10-31 23:17:27 +0200

    V3.0

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 
 app = Flask(__name__)
 app.secret_key = '12345678'  # Change this to a real secret key in production
@@ -19,6 +19,58 @@ users = {
         ]
     }
 }
+
+@app.route('/create_account', methods=['POST'])
+def create_account():
+    if 'username' not in session:
+        flash('You must be logged in to create an account.', 'error')
+        return redirect(url_for('index'))
+
+    username = session['username']
+    new_account_number = f"{int(users[username]['accounts'][-1]['account_number']) + 1:05d}"
+    users[username]['accounts'].append({
+        'account_number': new_account_number,
+        'balance': 0,
+        'transactions': []  # Initialize the transactions list
+    })
+    flash('New account created successfully!', 'success')
+    return redirect(url_for('dashboard'))
+
+@app.route('/transfer', methods=['POST'])
+def transfer():
+    if 'username' not in session:
+        flash('You must be logged in to transfer funds.', 'error')
+        return redirect(url_for('index'))
+
+    from_account_num = request.form.get('from_account')
+    to_account_num = request.form.get('to_account')
+    amount = float(request.form.get('amount'))
+
+    # Helper function to find an account by number across all users
+    def find_account_by_number(account_number):
+        for user_accounts in users.values():
+            for account in user_accounts['accounts']:
+                if account['account_number'] == account_number:
+                    return account
+        return None
+
+    from_account = find_account_by_number(from_account_num)
+    to_account = find_account_by_number(to_account_num)
+
+    if from_account and to_account and from_account['balance'] >= amount:
+        # Perform the transfer
+        from_account['balance'] -= amount
+        to_account['balance'] += amount
+
+        # Record the transactions
+        from_account.setdefault('transactions', []).append({'type': 'Transfer Out', 'amount': -amount})
+        to_account.setdefault('transactions', []).append({'type': 'Transfer In', 'amount': amount})
+
+        flash('Transfer successful!', 'success')
+    else:
+        flash('Transfer failed. Check account numbers and balance.', 'error')
+
+    return redirect(url_for('dashboard'))
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
--- test_app.py
+++ test_app.py
@@ -10,8 +10,11 @@ def app():
 def client(app):
     return app.test_client()
 
+def login_as_user1(client):
+    return client.post("/", data={'username': 'user1', 'password': 'pass1'})
+
 def test_login_success(client):
-    response = client.post("/", data={'username': 'user1', 'password': 'pass1'})
+    response = login_as_user1(client)
     assert response.status_code == 302
     assert '/dashboard' in response.headers['Location']
 
@@ -21,15 +24,41 @@ def test_login_failure(client):
 
 def test_dashboard_access(client):
     with client:
-        client.post("/", data={'username': 'user1', 'password': 'pass1'})
+        login_as_user1(client)
         response = client.get("/dashboard")
         assert response.status_code == 200
         assert b'00001' in response.data  # Check for account number
 
 def test_logout(client):
     with client:
-        client.post("/", data={'username': 'user1', 'password': 'pass1'})
+        login_as_user1(client)
         client.get('/logout')
         response = client.get('/dashboard')
         assert response.status_code == 302
         assert '/' in response.headers['Location']
+
+def test_create_account(client):
+    with client:
+        login_as_user1(client)
+        response = client.post('/create_account')
+        assert response.status_code == 302
+        assert '/dashboard' in response.headers['Location']
+        response = client.get('/dashboard')
+        assert b'00003' in response.data  # Assuming a new account number '00003' is created
+
+def test_transfer_funds(client):
+    with client:
+        login_as_user1(client)
+        # You need to modify this test to match how your transfer function works.
+        # This is just a placeholder for the idea.
+        response = client.post('/transfer', data={
+            'from_account': '00001',
+            'to_account': '00002',
+            'amount': '50'
+        })
+        assert response.status_code == 302
+        assert '/dashboard' in response.headers['Location']
+        response = client.get('/dashboard')
+        # Check if the balances have been updated accordingly
+        assert b'950' in response.data  # New balance in from_account
+        assert b'2550' in response.data  # New balance in to_account
